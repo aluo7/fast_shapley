4d6805a7706fe020bc5f52557c6e96d5407a0de5
Your Username <your_email@example.com>
2024-10-24 21:14:00 -0700

    [#6] Rename dir and notebook

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # fast_shapley
 1. `make build`
 2. `make run`
+
+note: opted for GradientExplainer rather than DeepSHAP due to lack of GELU/LayerNorm/Identity compatibility
--- utils/shap_utils.py
+++ utils/shap_utils.py
@@ -33,28 +33,15 @@ def map_shap_to_image(shap_values, img_size=224, patch_size=16):
     
     return upscaled_shap
 
-def visualize_shap(shap_values, original_image, patch_size=16, smooth=True, sigma=3, color_map='jet'):
+def visualize_shap(shap_values, original_image, patch_size=16, smooth=False, color_map='jet'):
     upscaled_shap = map_shap_to_image(shap_values[0], img_size=224, patch_size=patch_size)
-
     print(f"SHAP value range before normalization: min={upscaled_shap.min()}, max={upscaled_shap.max()}")
     
-    scaling_factor = 50  # manual scaling
-    upscaled_shap *= scaling_factor
-    print(f"SHAP value range after scaling: min={upscaled_shap.min()}, max={upscaled_shap.max()}")
+    normalized_shap = (upscaled_shap - upscaled_shap.min()) / (upscaled_shap.max() - upscaled_shap.min())
+    print(f"Normalized SHAP value range: min={normalized_shap.min()}, max={normalized_shap.max()}")
     
-    min_display_value = 0.05
-    max_display_value = 1.0
-    
-    clipped_shap = np.clip(upscaled_shap, min_display_value, max_display_value) # clipping
-    normalized_shap = (clipped_shap - min_display_value) / (max_display_value - min_display_value)
-    
-    print(f"Normalized SHAP value range: min={normalized_shap.min()}, max={normalized_shap.max()}") # normalized shap values
-    
-    gamma = 0.8
-    normalized_shap = np.power(normalized_shap, gamma)
-    
-    if smooth: # optionally apply smoothing
-        normalized_shap = scipy.ndimage.gaussian_filter(normalized_shap, sigma=sigma)
+    if smooth:  # gauss smoothing
+        normalized_shap = scipy.ndimage.gaussian_filter(normalized_shap, sigma=3)
     
     fig, ax = plt.subplots(1, 2, figsize=(12, 6))
     ax[0].imshow(original_image)
@@ -62,11 +49,10 @@ def visualize_shap(shap_values, original_image, patch_size=16, smooth=True, sigm
     ax[0].axis('off')
 
     ax[1].imshow(original_image)
-    im = ax[1].imshow(normalized_shap, cmap=color_map, alpha=0.4)
-    ax[1].set_title('Upscaled SHAP Overlay with Focused Clipping')
+    im = ax[1].imshow(normalized_shap, cmap=color_map, alpha=0.5)
+    ax[1].set_title('Upscaled SHAP Overlay without Clipping')
     ax[1].axis('off')
     
-    # Add a color bar to visualize the value range
     fig.colorbar(im, ax=ax[1], orientation='vertical')
     
     plt.tight_layout()
